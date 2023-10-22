0b0d2f82e83969ad7049331d8bb974893b704d7a
bud-hub <74277577+bud-hub@users.noreply.github.com>
2023-10-22 20:35:33 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 # 以下を「app.py」に書き込み
 import os
+import openai  # 追加: openaiモジュールをインポート
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
@@ -15,54 +16,50 @@ from langchain.prompts import MessagesPlaceholder
 openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 
 def create_agent_chain():
-  chat = ChatOpenAI(
-  model_name="gpt-3.5-turbo",
-  temperature==0.5,
-  streaming=True,
-  )
+    chat = ChatOpenAI(
+        model_name="gpt-3.5-turbo",
+        temperature=0.5,
+        streaming=True,
+    )
 
-  agent_kwargs = {
-    "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
-  }
+    agent_kwargs = {
+        "extra_prompt_messages": [MessagesPlaceholder(variable_name="memory")],
+    }
 
-  memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
+    memory = ConversationBufferMemory(memory_key="memory", return_messages=True)
 
-
-  tools = load_tools(["ddg-search", "wikipedia"])
-  return initialize_agent(
-    tools,
-    chat,
-    agent=AgentType.OPENAI_FUNCTIONS,
-    agent_kwargs=agent_kwargs,
-    memory=memory,
-  )
+    tools = load_tools(["ddg-search", "wikipedia"])
+    return initialize_agent(
+        tools,
+        chat,
+        agent=AgentType.OPENAI_FUNCTIONS,
+        agent_kwargs=agent_kwargs,
+        memory=memory,
+    )
 
 if "agent_chain" not in st.session_state:
     st.session_state.agent_chain = create_agent_chain()
 
-
 st.title("langchain-steremlit-app")
 
-if "messages"  not in st.session_state:
-  st.session_state.messages = []
+if "messages" not in st.session_state:
+    st.session_state.messages = []
 
 for message in st.session_state.messages:
-  with st.chat_message(message["role"]):
-    st.markdown(message["content"])
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
 
-prompt = st.chat_input("What is up?")
+prompt = st.text_input("What is up?")  # 修正: st.chat_inputをst.text_inputに変更
 
 if prompt:
-  st.session_state.messages.append({"role": "user", "content": prompt})
+    st.session_state.messages.append({"role": "user", "content": prompt})
 
-  with st.chat_message("user"):
-    st.markdown(prompt)
+    with st.chat_message("user"):
+        st.markdown(prompt)
 
+    with st.chat_message("assistant"):
+        callback = StreamlitCallbackHandler(st.container())
+        response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
+        st.markdown(response)
 
-
-  with st.chat_message("assistant"):
-    callback = StreamlitCallbackHandler(st.container())
-    response = st.session_state.agent_chain.run(prompt, callbacks=[callback])
-    st.markdown(response)
-
-  st.session_state.messages.append({"role": "assistant", "content": response})
+    st.session_state.messages.append({"role": "assistant", "content": response})
